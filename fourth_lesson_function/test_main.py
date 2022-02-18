4cdad1f62b2b1386d0330d16dcdf3113a7aeac62
vao <vao@maynitek.ru>
2022-02-18 23:06:37 +0500

    Добавил файл для тестов Написал первый тест, который прохоходит

--- fourth_lesson_function/test_main.py
+++ fourth_lesson_function/test_main.py
@@ -3,5 +3,22 @@ import os
 
 # Главный (он же единственный),
 # файл вашего решения называется main.py
-def test_name_is_main():
+def test_if_name_main():
     assert os.path.isfile('./main.py')
+
+# Главная функция, запустив которую начнется игра -
+# это функция с названием game() без параметров
+
+
+def test_if_name_game():
+    pass
+
+
+class TestClass:
+
+
+    @pytest.mark.parametrize("arg", ["a"])
+    def test_stuff(self, request, arg):
+        print("originalname:", request.node.originalname)
+        print("name:", request.node.name)
+        print("nodeid:", request.node.nodeid)
