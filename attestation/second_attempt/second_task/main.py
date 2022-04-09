e315e52db6721512124a6296675780828ad13611
vao <vao@maynitek.ru>
2022-04-09 22:56:23 +0500

    закончил, прогнал тесты, прогнал форматирование flask8, что смог, то поправил

--- attestation/second_attempt/second_task/main.py
+++ attestation/second_attempt/second_task/main.py
@@ -5,9 +5,62 @@ from typing import Any
 
 class TooMuchParametersException(Exception):
     """Create my exception."""
+
     def __init__(self: Any, message: str) -> None:
+        """Унаследуем из исключения."""
         super().__init__(message)
 
 
+def calculate(dictionary: dict):
+    """Calculate all combinations."""
+    atmospheric_list = []
+    total = []
+    for key in dictionary:
+        key_values = dictionary.get(key)
+        for item in key_values:
+            atmospheric_parameters = {key: item}
+            atmospheric_list.append(atmospheric_parameters)
+        total.append(atmospheric_list)
+        atmospheric_list = []
+
+    result = list(itertools.product(*total))
+    new_dict = {}
+    new_result = []
+    for item in result:
+        for x in item:
+            new_dict.update(x)
+        new_result.append(new_dict)
+        new_dict = {}
+
+    if len(new_result) > 100:
+        raise TooMuchParametersException("Too much parameters")
+    else:
+        return new_result
+
+
 if __name__ == '__main__':
-    print(calculate(a))
+    first_test = {
+        "атмосфера": ["кислородосодержащая"],
+        "населённость": ["растения"],
+        "размер": ["средний"],
+        "температура": ["средняя"]
+    }
+
+    second_test = {
+        "атмосфера": ["кислородосодержащая", "отсутствует"],
+        "размер": ["карлик", "великан"],
+        "температура": ["низкая", "средняя", "высокая"],
+        "посещалась ранее": ["да", "нет"]
+    }
+
+    bad_test = {
+        "атмосфера": ["кислородосодержащая", "отсутствует"],
+        "размер": ["карлик", "средний", "великан"],
+        "населённость": ["растения", "существа", "разумные существа", "нет"],
+        "температура": ["низкая", "средняя", "высокая"],
+        "посещалась ранее": ["да", "нет"]
+    }
+
+    print(calculate(first_test))
+    print(calculate(second_test))
+    print(calculate(bad_test))
