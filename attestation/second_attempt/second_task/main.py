0635a896e4537332323874116b6358eda79bcd97
vao <vao@maynitek.ru>
2022-04-09 19:59:45 +0500

    написал свое исключение, добавил пример

--- attestation/second_attempt/second_task/main.py
+++ attestation/second_attempt/second_task/main.py
@@ -1,1 +1,13 @@
 """Task #2 for attestation."""
+import itertools
+from typing import Any
+
+
+class TooMuchParametersException(Exception):
+    """Create my exception."""
+    def __init__(self: Any, message: str) -> None:
+        super().__init__(message)
+
+
+if __name__ == '__main__':
+    print(calculate(a))
